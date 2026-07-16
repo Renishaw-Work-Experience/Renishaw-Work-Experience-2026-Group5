c9ec36d94ac373bf9eaf482b7bcaace80ae9b791
jamesl06-notwork <james_lane06@outlook.com>
2026-07-16 08:45:13 +0100

    Add files via upload
    Fixes: The port number used is restricted on my PC - Switched to one I know works
    
    Changed unread messages to not iterate the clear messages line - I assumed that all of the messages should be printed before clearing
    
    Largest fix - The current pattern creates a new socket connection every time an option is selected - this is not exactly best practices.
    What actually causes the breakage is that the server was not configured to accept any new connections - after a new option was picked a disconnection occurs.

--- Client-interaction-with-database.py
+++ Client-interaction-with-database.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
+PORT = 5000  # The port used by the server
 
 username = input(str("Please enter a username: "))
 
